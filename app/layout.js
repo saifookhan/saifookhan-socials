a7b5b5628c92bb86eb2b6ee7b5d126f8aa512ec1
aashir12 <aashirnawaz5@gmail.com>
2024-08-27 13:37:17 +0500

    added clarity

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kdz2zgnwib");
+        `}
+      </Script>
       <body className={poppins.className}>
         <NextTopLoader
           color="hsl(229 100% 62%)"
